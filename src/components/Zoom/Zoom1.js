31df0cd73333fe83a7bedac5e4c99175246f1d87
Paweł Kopycki <pawel.kopycki@qed.pl>
2020-12-11 09:47:38 +0100

    cleaning

--- src/components/Zoom/Zoom1.js
+++ src/components/Zoom/Zoom1.js
@@ -112,7 +112,6 @@ export default function BarChart(props) {
     })
 
 
-
     const changemode = (e) => {
 
         // console.log(e.target.value)
@@ -247,17 +246,10 @@ export default function BarChart(props) {
             .attr("clip-path", "url(#clip)")
 
         const updateChart = (e) => {
-            // console.log(e)
-
-            // if (!newX || !newY) {
 
             newX = e.transform.rescaleX(xScale2)
             newY = e.transform.rescaleY(yScale)
-            // console.log(newX.domain())
-            // } else {
-            //     // newX = chartOptions.newX
-            //     // newY = chartOptions.newY
-            // }
+
             xAxis
                 .call(d3.axisBottom(newX).tickSize(-height))
 
@@ -277,31 +269,21 @@ export default function BarChart(props) {
 
                 scatter
                     .selectAll(`.horizontal${j}`)
-
-                    // .attr('con', (d) => console.log(select(d.target), 'oko'))
-                    // eslint-disable-next-line no-loop-func
                     .attr('x1', (d, i) => newX(i))
                     // eslint-disable-next-line no-loop-func
                     .attr('x2', (d, i) => newX(i))
                     // eslint-disable-next-line no-loop-func
                     .attr('y1', (d, i) => newY(props.data.instance[j].Low[i]))
                     .attr('y2', (d, i) => newY(props.data.instance[j].High[i]))
-                    // .attr('stroke-width', e.transform.k/2)
-                    // .attr('stroke', 'black')
                     .attr('stroke-width', e.transform.k > 4 ? 0.1 * e.transform.k : 1);
-                // console.log(lines)
+
             }
             for (let j = 0; j < props.data.instance.length; j++) {
                 const rawData2 = props.data.instance[j]
                 // console.log(rawData2)
                 scatter
                     .selectAll(`.candle${j}`)
-                    // .attr('class', 'xman')
-
-                    // eslint-disable-next-line no-loop-func
                     .attr('x', (d, i) => xScale.bandwidth() > 8 || e.transform.k > 1.5 ? newX(i) - 4 : newX(i) - xScale.bandwidth() / 2)
-                    // eslint-disable-next-line no-loop-func
-                    // .attr('id', (d, i) => j * i + i)
                     // eslint-disable-next-line no-loop-func
                     .attr('y', (d, i) => rawData2['Open'][i] < rawData2['Close'][i] ? newY(rawData2['Close'][i]) : newY(rawData2['Open'][i]))
                     // eslint-disable-next-line no-loop-func
@@ -316,7 +298,6 @@ export default function BarChart(props) {
 
             }
 
-            // }
             scatter
                 .selectAll('.mark')
                 .data(marks)
@@ -331,16 +312,14 @@ export default function BarChart(props) {
                 .data(marks)
                 .attr('x', d => newX(d.x) + 5)
                 .attr('y', d => newY(d.y) + 10)
-            // .attr("font-size", d => newY(d.y))
+
 
             scatter
                 .selectAll('.line-graph')
                 .attr("d", d3.line()
-
                     .x((d, i) => newX(i))
                     .y((d) => newY(d))
                 )
-            // .attr('stroke-width', e.transform.k/3)
 
             scatter
                 .selectAll('.dot')
@@ -348,15 +327,10 @@ export default function BarChart(props) {
                 .attr("cy", (d) => newY(d))
                 // .attr('r', e.transform.k/2)
                 .attr('r', 0.4 * e.transform.k)
-            // for (let mark of marks) {
-            //     console.log(mark)
-            //     mark.setAttributeNS(null,'x', newX(mark.attributes.x.value * 1))
-            // }
-
-            // .attr('y', (d, i) => rawData.Open[i] < rawData.Close[i] ? newY(rawData.Close[i]) : newY(rawData.Open[i]))
-
 
         }
+
+
         const zoom = d3.zoom()
             .scaleExtent([-2, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
             .extent([[0, 0], [width, height]])
@@ -372,7 +346,6 @@ export default function BarChart(props) {
 
 
         const updateBrush = ({selection}) => {
-
 
             if (!newX) {
                 newX = xScale2
@@ -417,9 +390,7 @@ export default function BarChart(props) {
                 .attr('x2', (d, i) => newX(i))
                 .attr('y1', (d, i) => newY(rawData.Low[i]))
                 .attr('y2', (d, i) => newY(rawData.High[i]))
-            // .attr('stroke-width', e.transform.k/2)
-            // .attr('stroke', 'black')
-            // .attr('stroke-width', e.transform.k > 4 ? 0.1 * e.transform.k : 1)
+
             scatter
                 .selectAll('rect')
                 // .attr('x', (d, i) => newX(i ) -(x(e.transform.k))/2)
@@ -429,8 +400,7 @@ export default function BarChart(props) {
                 .transition().duration(300)
                 .attr('y', (d, i) => rawData.Open[i] < rawData.Close[i] ? newY(rawData.Close[i]) : newY(rawData.Open[i]))
                 .attr('height', (d, i) => {
-                    // console.log(yScale(props.data.Close[i] - d))
-                    // return props.data.Open[i] < props.data.Close[i]?yScale(0)-yScale(props.data.Close[i]-d):yScale(0) - yScale(d-props.data.Close[i]) })
+
                     if (rawData.Open[i] < rawData.Close[i]) {
                         return (newY(0) - newY(rawData.Close[i] - d))
                     } else if (rawData.Open[i] > rawData.Close[i]) {
@@ -446,19 +416,16 @@ export default function BarChart(props) {
                     .x((d, i) => newX(i))
                     .y((d) => newY(d))
                 )
-            // .attr('stroke-width', e.transform.k/3)
+
 
             scatter
                 .selectAll('.dot')
                 .attr("cx", (d, i) => newX(i))
                 .attr("cy", (d) => newY(d))
-            // .attr('r', e.transform.k/2)
-            // .attr('r', 0.4 * e.transform.k)
+
             scatter
                 .call(d3.brush().move, null)
-            // xcale2.domain([selection[0][0], selection[1][0]])
-            // yScale.domain([selection[1][1], selection[0][1]])
-            // console.log(xAxis)
+
             chartChange('newX', newX)
             chartChange('newY', newY)
             setMode('Free')
@@ -483,9 +450,7 @@ export default function BarChart(props) {
 
                         container
                             .append('rect')
-                            // .style('opacity', 0.01)
                             .attr('class', 'mark')
-
                             .attr('fill', mark.color)
                             .attr('opacity', 0.2)
                             .attr('x', newX(mark.x))
@@ -520,19 +485,14 @@ export default function BarChart(props) {
                 let rects = scatter
                     .selectAll(`.candle${i}`)
 
-                // drawMarks(marks)
                 for (let rect of rects) {
-                    // console.log(extent[0][0], rect.x.animVal.value, extent[1][0])
-                    // console.log(rect)
-                    // console.log(selection[0])
-                    // const isBrushed = selection && selection[0] && selection[1] && selection[0][0] <= rect.x.animVal.value && selection[1][0] >= rect.x.animVal.value
+
                     const isBrushed = selection && selection[0] && selection[1] && selection[0][0] <= rect.x.animVal.value && selection[1][0] >= rect.x.animVal.value && selection[0][1] <= rect.y.animVal.value && selection[1][1] >= rect.y.animVal.value + rect.height.animVal.value
 
                     if (isBrushed) {
 
-                        // console.log(rect.id)
                         if (arr.has(rect)) {
-                            // console.log('dupa')
+
                             arr.delete(rect)
                             select(rect)
                                 .attr('stroke', '#6dcd65')
@@ -546,8 +506,6 @@ export default function BarChart(props) {
 
                             arr.add(rect)
                             markReact.add(rect)
-                            // console.log(arr)
-
 
                         }
 
@@ -642,7 +600,6 @@ export default function BarChart(props) {
                     .append('rect')
                     .attr('class', `candle${index}`)
                     .attr('opacity', 1)
-                    // .attr('id', (d, i) => index * rawData.Open.length + i)
                     .attr('id', (d, i) => color + i)
                     .on("click", (d) => {
                         if (arr.has(d.target)) {
@@ -692,25 +649,17 @@ export default function BarChart(props) {
 
                 if (chartOptions.arr) {
 
-                    // for (let mark of marks) {
-
-
                     for (let markRect of myArr) {
                         if (markRect.id === rects[i].id && markRect.attributes.isMark.value === 'true') {
                             select(rects[i])
                                 .attr('stroke', 'red')
                                 .attr('stroke-width', 1)
-                                // .attr('id', (d, i) => color+i)
                                 .attr('isMark', markRect.attributes.isMark.value)
-                            // newArr.add(select(rects[i])._groups[0][0])
                         }
                     }
-                    // }
                 }
 
             }
-            // arr = newArr
-            // chartChange('arr', newArr)
         }
 
         for (let i = 0; i < props.data.instance.length; i++) {
@@ -720,8 +669,7 @@ export default function BarChart(props) {
         }
         drawMarks(chartOptions.marks);
         createMarks()
-        // setRefresh(!refresh)
-        // console.log(chartOptions, 'ooo')
+
         switch (mode) {
             case "Free":
                 // console.log('free');
@@ -745,8 +693,9 @@ export default function BarChart(props) {
                     .call(d3.brush()
                         .extent([[0, 0], [width, height]])
                         .on('end', updateBrush))
-
-
+                break;
+            default:
+                console.log('!!!!')
         }
 
     }
@@ -785,32 +734,17 @@ export default function BarChart(props) {
                 tagDialogOPen={tagDialogOpen}
             />
 
-            {/*<button onClick={() => console.log((Array.from(data)))}>0000</button>*/}
             <div style={{marginLeft: 40}}><b>Task ID: {taskId}</b></div>
             <div style={{display: 'flex'}}>
 
                 <div id={"kotek"} ref={ref1} style={{userSelect: "none"}}>
-                    {/*<svg ref={ref1}/>*/}
+
                 </div>
 
-                {/*<div>*/}
-                {/*    <Select*/}
-                {/*        labelId="demo-simple-select-label"*/}
-                {/*        id="demo-simple-select"*/}
-                {/*        // value={0}*/}
-                {/*        onChange={(e) => groupData(e.target.value)}*/}
-                {/*    >*/}
-                {/*        <MenuItem value={0}>0</MenuItem>*/}
-                {/*        <MenuItem value={3}>3</MenuItem>*/}
-                {/*        <MenuItem value={7}>7</MenuItem>*/}
-                {/*        <MenuItem value={9}>9</MenuItem>*/}
-                {/*    </Select>*/}
-                {/*</div>*/}
+
                 <div>
                     <Paper>
-                        {/*<Typography variant="subtitle2">*/}
-                        {/*    <b>Charts</b>*/}
-                        {/*</Typography>*/}
+
                         {chartOptions.charts.map((data, i) =>
 
 
@@ -833,7 +767,6 @@ export default function BarChart(props) {
                                 }}
                                      onClick={() => setChartOpen(true)}
                                 >
-                                    {/*{data}*/}
                                 </div>
                                 <div style={{cursor: 'pointer'}}
                                      onClick={() => setVisible(chartOptions.charts, 'charts', i)}>
@@ -865,7 +798,7 @@ export default function BarChart(props) {
                                                     fill={'#fff'}
                                                 />
                                             </>}
-                                            {data.line === 3&&<>
+                                            {data.line === 3 && <>
                                                 <circle
                                                     r="4.1208663"
                                                     cy="283.88895"
@@ -891,8 +824,7 @@ export default function BarChart(props) {
                 </div>
                 <div>
                     <Paper style={{marginLeft: 10}}>
-                    {chartOptions.marks.map((data, i) =>
-
+                        {chartOptions.marks.map((data, i) =>
 
 
                             <div style={{display: "flex", alignItems: 'center'}}>
@@ -922,11 +854,7 @@ export default function BarChart(props) {
                     </Paper>
                 </div>
             </div>
-            {/*<button onClick={() => {*/}
-            {/*    create();*/}
-            {/*    console.log(chartOptions)*/}
-            {/*    setOpen(!open)*/}
-            {/*}}>ZZZZZZ</button>*/}
+
             <FormControl component="fieldset">
                 {/*<FormLabel component="legend">labelPlacement</FormLabel>*/}
                 <RadioGroup row aria-label="position" style={{marginLeft: 40}} name="position" onChange={changemode}
@@ -936,33 +864,26 @@ export default function BarChart(props) {
                         control={<Radio color="primary"/>}
                         label="Free"
                         labelPlacement="top"
-                        // onClick={changemode}
+
                     />
                     <FormControlLabel
                         value="Zoom"
                         control={<Radio color="primary"/>}
                         label="Zoom"
                         labelPlacement="top"
-                        // onClick={changemode}
+
                     />
                     <FormControlLabel
                         value="Mark"
                         control={<Radio color="primary"/>}
                         label="Mark"
                         labelPlacement="top"
-                        // onClick={changemode}
+
                     />
-                    {/*<FormControlLabel*/}
-                    {/*    value="Click"*/}
-                    {/*    control={<Radio color="primary" />}*/}
-                    {/*    label="Click"*/}
-                    {/*    labelPlacement="top"*/}
-                    {/*    // onClick={changemode}*/}
-                    {/*/>*/}
-                    {/*<FormControlLabel value="end" control={<Radio color="primary" />} label="End" />*/}
+
                 </RadioGroup>
             </FormControl>
-            {/*<input type={'checkbox'} onChange={()=>{setLine(!line);}}/>*/}
+
             <Button style={{marginRight: 5}} variant="contained" color="primary"
                     onClick={() => setOpen(!open)}>TAG</Button>
             <Button style={{marginRight: 5}} variant="contained" color="primary" onClick={() => {
@@ -973,22 +894,22 @@ export default function BarChart(props) {
                 setMode('Free')
                 setFull(!full)
 
-                // reate()
+
             }}>FULL</Button>
 
 
-            <Button style={{marginRight: 5}} variant="contained" color="secondary" onClick={() => {
-                setChartOptions({
-                    newY: undefined,
-                    newX: undefined,
-                    dataMax: undefined,
-                    xAsis: undefined,
-                    arr: undefined,
-                    marks: undefined,
-                    charts: []
-                })
-                setFull(!full)
-            }}>RESET</Button>
+            {/*<Button style={{marginRight: 5}} variant="contained" color="secondary" onClick={() => {*/}
+            {/*    setChartOptions({*/}
+            {/*        newY: undefined,*/}
+            {/*        newX: undefined,*/}
+            {/*        dataMax: undefined,*/}
+            {/*        xAsis: undefined,*/}
+            {/*        arr: undefined,*/}
+            {/*        marks: undefined,*/}
+            {/*        charts: []*/}
+            {/*    })*/}
+            {/*    setFull(!full)*/}
+            {/*}}>RESET</Button>*/}
 
 
         </div>
